beb4112b069f39e7d1d99a72cfe497cb0e66bbad
Onsomujosh <joshuaonsomu@gmail.com>
2024-02-16 01:21:12 +0300

    🎉 pycodestyle

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -37,7 +37,7 @@ class Auth:
         """Gets the current user from the request.
         """
         return None
-    
+
     def session_cookie(self, request=None) -> str:
         """Gets the value of the cookie named SESSION_NAME.
         """
